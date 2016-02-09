1d9997ee8b9a4e69150a440d4003d3cb703ce025
making <makingx@gmail.com>
2016-02-09 15:51:27 +0900

    Add download script

---
+++ download-and-add-blob.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+VERSION=1.8.0_71
+FILENAME=openjdk-jdk-trusty-${VERSION}.tar.gz
+mkdir -p tmp
+pushd tmp
+wget https://download.run.pivotal.io/openjdk-jdk/trusty/x86_64/openjdk-${VERSION}.tar.gz -O ${FILENAME}
+popd
+bosh add blob  tmp/${FILENAME} jdk
