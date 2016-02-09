0f424bdd725387416279ce1d2b8fc38059b94204
Toshiaki Maki <makingx@gmail.com>
2016-02-09 22:01:09 +0900

    Fix script

--- download-and-add-blob.sh
+++ download-and-add-blob.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 VERSION=1.8.0_71
 FILENAME=openjdk-jdk-trusty-${VERSION}.tar.gz
-mkdir -p tmp
-pushd tmp
+mkdir -p ../../tmp
+pushd ../../tmp
 wget https://download.run.pivotal.io/openjdk-jdk/trusty/x86_64/openjdk-${VERSION}.tar.gz -O ${FILENAME}
+bosh add blob tmp/${FILENAME} jdk
 popd
-bosh add blob  tmp/${FILENAME} jdk
+
